78f735f374a247d5f7f49cf3fa5845d9f913309f
ssp <swalehapeerzade@gmail.com>
2022-07-06 15:29:14 -0400

    commit

--- tests/test_math.py
+++ tests/test_math.py
@@ -12,6 +12,7 @@ import pytest
 # The most basic test function
 #----------------------------------------------------
 
+@pytest.mark.math
 def test_one_plus_one():
     assert 1 + 1 == 2
     
@@ -20,6 +21,7 @@ def test_one_plus_one():
 # A test function to show assertion introspection
 #----------------------------------------------------
 
+@pytest.mark.math
 def test_one_plus_two():
     a = 1
     b = 2
@@ -31,6 +33,7 @@ def test_one_plus_two():
 # A test function that verifies the exception
 #---------------------------------------------------- 
 
+@pytest.mark.math
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         num = 1/0
@@ -43,6 +46,7 @@ def test_divide_by_zero():
 
 # multiplication test ideas: Equivalence classes for test case inputs 
 
+
 products = [
     (2, 3, 6),        # two positive numbers
     (1, 99, 99),      # Identity
@@ -53,7 +57,7 @@ products = [
  
 ]
 
-
+@pytest.mark.math
 @pytest.mark.parametrize('a, b, product', products) 
 def test_multiplication(a, b, product):
     assert a * b == product
